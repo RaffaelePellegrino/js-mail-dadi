9fa646f0d05f3bedf34c20f23513a115e028eff2
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2024-11-04 16:24:20 +0100

    primi 2 esercizi

---
+++ esercizi.js
@@ -0,0 +1,43 @@
+// const list=["ksdanf@gmail.com","michelecaruso@gmail.com","paoloserace@gmail.com"]
+
+// let mailUtente=prompt("inserisci la tua mail:")
+
+// isIn = false
+// for(let i = 0; i <= list.length; i++){
+//     if(list[i] === mailUtente){
+//         isIn=true
+//     }
+// }
+
+// if(!isIn){
+//     console.log("non sei nella lista")
+// }else{
+//     console.log("sei nella lista")
+// }
+// 
+
+
+// secondo esercizio
+
+// function tiro(min,max){
+//     risultato=(Math.floor(Math.random()* (max-min))+min);
+//     return risultato;
+// }
+
+// let tiroUtente= tiro(1,100)
+
+// let tiroComputer= tiro(1,100)
+
+// if (tiroUtente > tiroComputer){
+//     console.log(tiroComputer + " " + "il tiro dell'avversario")
+//     console.log(tiroUtente+ " " + "il tuo tiro")
+//     console.log("Complimenti,hai vinto")
+// }else if (tiroUtente < tiroComputer){
+//     console.log(tiroComputer + " "+"il tiro dell'avversario")
+//     console.log(tiroUtente+ " "+"il tuo tiro")
+//     console.log("Hai perso")
+// }else{
+//     console.log(tiroComputer +" "+ "il tiro dell'avversario")
+//     console.log(tiroUtente+ " "+"il tuo tiro")
+//     console.log("E' un pareggio")
+// }
